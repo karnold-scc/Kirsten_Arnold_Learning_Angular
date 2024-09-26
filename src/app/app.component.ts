c51dbe3f416393614357d15dbed14913198a3bf4
Kiri <silverpool1441@gmail.com>
2024-09-26 13:53:04 -0400

    Completed assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {User} from "./shared/models/user";
 import {CommonModule} from "@angular/common";
-import {A2} from "./shared/models/Book";
+import {Book} from "./shared/models/Book";
 import {ContentCardComponent} from "./content-card/content-card.component";
 import {BookListComponent} from "./book-list/book-list.component";
-interface IContent{
-    id:number,
-    imageUrl:string,
-    body:string,
-    type:string
 
-}
-// @ts-ignore
-// @ts-ignore
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -26,24 +18,5 @@ interface IContent{
 export class AppComponent {
   title?: "";
 
-contentArray:IContent[] = [
-  {
-    id: 1,
-    imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
-    body: 'This is the body of the content' ,
-   type: 'news'
-  },
-  {
-    id: 2,
-    imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
-    body: 'This is the body of the second content' ,
-    type: 'news'
-  },
-  {
-    id: 3,
-    imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
-    body: 'This is the body of the last content' ,
-    type: 'news'
-  }
-];
+
 }
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import {Book} from "../shared/models/Book";
 import {BookListitemComponent} from "../book-listitem/book-listitem.component";
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   imports: [
-    BookListitemComponent
+    BookListitemComponent,
+    NgForOf,
+    NgClass
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
