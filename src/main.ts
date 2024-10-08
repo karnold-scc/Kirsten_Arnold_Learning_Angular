03305dc678cb70ec20a3dae874d1419157b04a0e
W0863060 <ka147@myscc.ca>
2024-10-08 10:19:26 -0400

    Completed step 5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {BookListitemComponent} from "./app/book-listitem/book-listitem.component";
+import {BookListComponent} from "./app/book-list/book-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path:'', redirectTo: '/students', pathMatch: 'full'}, //default route
+  { path: 'book', component: BookListComponent },
+  { path: 'books/:id', component: BookListitemComponent },
+  // {path:'modify-student', component: ModifyBookComponent},
+  // {path: '**', component:PageNotFoundComponent}//Wildcard route for a 404 page
+];
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
