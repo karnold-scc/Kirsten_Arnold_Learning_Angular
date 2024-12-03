eb492a2f9edec8a16257e6c3573803925a7e8566
Kirsten <ka147@myscc.ca>
2024-12-03 10:55:26 -0500

    Re Uploaded Directive Files

---
+++ src/app/directives/app-focus-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AppFocusHighlightDirective } from './app-focus-highlight.directive';
+
+describe('AppFocusHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AppFocusHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
